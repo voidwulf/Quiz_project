1e2e75a26afb127d88442d375aac94fe7b837859
Martin Starčević <martin.starcevic989@gmail.com>
2021-10-16 14:27:42 +0000

    define starting code for question + awnsers + image loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,25 @@ const questions = [
     availableAwnsers: ["Cuba", "Florida", "Yemen", "Spain"]
 }
 ]
+
+
+fetch("assets/data/questions.json")
+.then(res => res.jeson())
+
+function createHTML (questions){
+    questions.forEach(question => {
+        const questionHTML = document.createElement("div");
+        const awnsers = question.availableAwnsers.forEach (q => {
+            return `<button>{q}</button>`
+        });
+    })
+    questionHTML.innerHTML = `
+    <img src = "${question.image}">
+    <button>awensers</button>
+    `
+    questionHTML.insertAdjecentElement('beforeend', awnsers)
+    questionHTML.insertAdjecentElement('beforeend', awnsers)
+    console.log(image)
+}
+
+
