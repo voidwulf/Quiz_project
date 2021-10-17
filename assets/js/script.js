8e13314825b43afb604b21d6035681e5ed8cb755
Martin Starčević <martin.starcevic989@gmail.com>
2021-10-17 23:41:08 +0000

    Add intro structure for quiz, add basic function sturcture in js file to start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
 /* Ferch questions from json file */
 const questions = fetch("assets/data/questions.json").then(res => res.json())
 
-function createHTML (questions){
-    questions.forEach(question => {
+
+
+/* Function that creates structure for quiz*/
+function createHTML (){
+    let quizSpace = document.getElementById("quizArea")
+    quizSpace.innerHTML = `
+    <p id="question"></p>
+    <button class='question'></button>
+    <button class='question'></button>
+    <button class='question'></button>
+    <button class='question'></button>
+    <button id='submit'></button>
+
+`;
+}
+/*Function that hides intro page and shows Quzi HTML structure */
+function hideIntro (){
+    let intro = document.getElementById("intro")
+    intro.classList.add('hidden') 
+    createHTML(questions)
+}
+
+
+function pullQuestions (qst){
+    qst.forEach(question => {
         const questionHTML = document.createElement("div");
         const awnsers = question.availableAwnsers.forEach (q => {
             return `<button>{q}</button>`
@@ -17,4 +40,8 @@ function createHTML (questions){
     console.log(image)
 }
 
+
+function initatieQuiz() {
+
+}
 
